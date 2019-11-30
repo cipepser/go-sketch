e952e601dbc42b64ab6525731079f27af8e0f74b
cipepser <respepic@gmail.com>
2019-12-01 01:47:51 +0900

    refactoring DoubleHashing

--- sketch/sketch.go
+++ sketch/sketch.go
@@ -3,7 +3,6 @@ package sketch
 import (
 	"github.com/cipepser/go-sketch/util"
 	"math"
-	"strconv"
 )
 
 const (
@@ -24,46 +23,28 @@ func NewSketch() *Sketch {
 
 // TODO: write a comment
 func (s *Sketch) Add(elem string) error {
-	hash := util.CalcMD5Hash(elem)
-	hashA := hash[:int(len(hash)/2)]
-	hashB := hash[int(len(hash)/2):]
-
-	u64HashA, err := strconv.ParseUint(hashA, 16, 64)
-	if err != nil {
-		return err
-	}
-	u64HashB, err := strconv.ParseUint(hashB, 16, 64)
-	if err != nil {
-		return err
-	}
-
 	for i := 0; i < k; i++ {
-		s[i][util.DoubleHashing(u64HashA, u64HashB, uint64(i), N)] += 1
+		h, err := util.DoubleHashing(elem, i, N)
+		if err != nil {
+			return err
+		}
+		s[i][h] += 1
 	}
 
 	return nil
 }
 
 // TODO: write a comment
-// TODO: implement Cardinality
-// TODO: Addとの共通部分を抜き出して関数化する
 func (s *Sketch) Cardinality(elem string) (int, error) {
 	min := math.MaxInt64
-	hash := util.CalcMD5Hash(elem)
-	hashA := hash[:int(len(hash)/2)]
-	hashB := hash[int(len(hash)/2):]
-
-	u64HashA, err := strconv.ParseUint(hashA, 16, 64)
-	if err != nil {
-		return 0, err
-	}
-	u64HashB, err := strconv.ParseUint(hashB, 16, 64)
-	if err != nil {
-		return 0, err
-	}
 
 	for i := 0; i < k; i++ {
-		v := s[i][util.DoubleHashing(u64HashA, u64HashB, uint64(i), N)]
+		h, err := util.DoubleHashing(elem, i, N)
+		if err != nil {
+			return 0, err
+		}
+
+		v := s[i][h]
 		if v < min {
 			min = v
 		}
--- util/util.go
+++ util/util.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"math/big"
+	"strconv"
 )
 
 // CalcMD5Hash calculates hash value of the string input by MD5.
@@ -16,8 +17,31 @@ func CalcMD5Hash(str string) string {
 }
 
 // DoubleHashing calculates hash value by double-hashing.
+// elem: input which is hashed
+// i: i-th hash value
+// r: the size of hash table, used by calculating modulus
+func DoubleHashing(elem string, i int, r uint64) (uint64, error) {
+	hash := CalcMD5Hash(elem)
+	hashA := hash[:int(len(hash)/2)]
+	hashB := hash[int(len(hash)/2):]
+
+	u64HashA, err := strconv.ParseUint(hashA, 16, 64)
+	if err != nil {
+		return 0, err
+	}
+	u64HashB, err := strconv.ParseUint(hashB, 16, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	h := doubleHashing(u64HashA, u64HashB, uint64(i), r)
+
+	return h, nil
+}
+
+// doubleHashing calculates hash value by double-hashing.
 // h = hashA + n * hashB % r
-func DoubleHashing(hashA, hashB uint64, n, r uint64) uint64 {
+func doubleHashing(hashA, hashB uint64, n, r uint64) uint64 {
 	a := big.NewInt(0).SetUint64(hashA)
 	b := big.NewInt(0).SetUint64(hashB)
 	mul := big.NewInt(0).SetUint64(n)
